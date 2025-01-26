3fb467477c66f52c12d461669275c684d2eb4e5e
Raju Methuku <raju.methuku@agoda.com>
2025-01-26 17:15:35 +0700

    fixed lint issues

--- backend/src/test/java/com/finclutech/dynamic_form_manager/DynamicFormManagerCucumberTestRunner.java
+++ backend/src/test/java/com/finclutech/dynamic_form_manager/DynamicFormManagerCucumberTestRunner.java
@@ -6,9 +6,27 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
-@ContextConfiguration(classes = com.finclutech.dynamic_form_manager.config.cucumber.CucumberSpringConfiguration.class)
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features", // Path to feature files
+        glue = "com.finclutech.dynamic_form_manager.steps", // Path to step definitions
+        plugin = {
+                "pretty", // Console output
+                "json:target/cucumber-report.json", // JSON report
+                "html:target/cucumber-reports.html" // HTML report
+        },
+        monochrome = true // Clean console output
+)
+@ContextConfiguration(classes = com.finclutech.dynamic_form_manager.config.cucumber.CucumberSpringConfiguration.class)
 public class DynamicFormManagerCucumberTestRunner {
 }
